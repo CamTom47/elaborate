cd0c16cba2d0756bf778340704d255211f55a8b5
Cameron Thomas <CamThomas47@gmail.com>
2025-12-18 15:00:20 -0500

    working through env configs

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 
-dotenv.config();
+if(process.env.NODE_ENV !== 'production'){
+    dotenv.config();
+}
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server started listening on PORT:${PORT}`)
 })
