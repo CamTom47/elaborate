47f1a59be63cc79861370c2ef1d3b74285fa7a4f
Cameron Thomas <CamThomas47@gmail.com>
2025-12-18 15:20:00 -0500

    logging

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ if(process.env.NODE_ENV !== 'production'){
     dotenv.config();
 }
 
+
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('PORT:', process.env.PORT);
+console.log('MAILGUN_API_KEY:', !!process.env.MAILGUN_API_KEY);
+console.log('MAIL_DOMAIN:', process.env.MAIL_DOMAIN);
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
