95995975abf5dd1aeb16fe9eaa3bced97195c7c8
Cameron Thomas <CamThomas47@gmail.com>
2025-12-18 13:29:09 -0500

    configured cors

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,29 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
-import messageRouter from './routes/messages.js'
-
+import messageRouter from "./routes/messages.js";
 
 const app = express();
 const router = express.Router();
 app.use(helmet());
-app.use(cors());
+
+const allowedOrigins = [
+	"https://frameworksdev.com", //production frontend
+	"http://localhost:5173", //development fronted
+];
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			//allow non-browser tools (no origin)
+			if (!origin) return callbacked(null, true);
+			if (allowedOrigins.includes(origin)) return callback(null, true);
+			return callback(new Error("Not allowed by CORS"));
+		},
+		credentials: true,
+	})
+);
+
 app.use(express.json());
-app.use('/message', messageRouter)
+app.use("/message", messageRouter);
 
 export default app;
