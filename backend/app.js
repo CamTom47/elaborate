2c4caab8ccdf0765ea3e55d89270cab79fa615a7
Cameron Thomas <CamThomas47@gmail.com>
2025-12-18 19:20:17 -0500

    testing

--- backend/app.js
+++ backend/app.js
@@ -30,19 +30,19 @@ app.use("/api/message", messageRouter);
 
 // Handle 404 errors. This will match everything
 app.use((req, res, next) => {
-    return next(new NotFoundError())
-})
+	return next(new NotFoundError());
+});
 
 //Generic error handler; anything unhandled goes here
-app.use( (err, req, res, next) => {
-    if(process.env.NODE_ENV !== 'test') console.error(err.stack);
-    const status = err.status || 500;
-    const message = err.message;
+app.use((err, req, res, next) => {
+	console.log(req);
+	if (process.env.NODE_ENV !== "test") console.error(err.stack);
+	const status = err.status || 500;
+	const message = err.message;
 
-    return res.status(status).json({
-        error: {message, status}
-    })
-})
-
+	return res.status(status).json({
+		error: { message, status },
+	});
+});
 
 export default app;
